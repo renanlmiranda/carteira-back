5df90c6faf780c59eb3c2832c6d5dffb7b8fc4f6
Renan Miranda <renanlmiranda@gmail.om>
2022-11-08 00:36:03 -0300

    add mux

--- routes/routes.go
+++ routes/routes.go
@@ -4,10 +4,12 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/renanlmiranda/carteira-back/controllers"
 )
 
 func HandleRequest() {
-	http.HandleFunc("/", controllers.Home)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home)
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
